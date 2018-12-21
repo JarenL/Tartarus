a4c45c7fb3c801dd59c71dbdf6f56abca3578896
Jaren Lynch <jaren.d.lynch@gmail.com>
2018-12-21 12:03:30 -0800

    Updates

---
+++ precache-manifest.fe94362db3518c51777a3d0ae5fd7b60.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c6b6c1ff0d770a41f0b",
+    "url": "./static/css/main.9397b9ad.chunk.css"
+  },
+  {
+    "revision": "3c6b6c1ff0d770a41f0b",
+    "url": "./static/js/main.3c6b6c1f.chunk.js"
+  },
+  {
+    "revision": "44e683032388b1531139",
+    "url": "./static/js/1.44e68303.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "1cd1aa124ab22433a697eea7e91cdcb2",
+    "url": "./static/media/Oswald-300.1cd1aa12.woff2"
+  },
+  {
+    "revision": "55bb45b64b791d306a39e3af9791216a",
+    "url": "./static/media/Oswald-300.55bb45b6.woff"
+  },
+  {
+    "revision": "29b3a057ac523422b12afa8f7cf692f7",
+    "url": "./static/media/Oswald-regular.29b3a057.woff2"
+  },
+  {
+    "revision": "4cd42bac8ebc77f27c60ce7ffe3bb975",
+    "url": "./static/media/Oswald-regular.4cd42bac.woff"
+  },
+  {
+    "revision": "d6069ae58a6b125b1da3f6bd32ec4a12",
+    "url": "./static/media/Oswald-300.d6069ae5.eot"
+  },
+  {
+    "revision": "31adcb0ebce77e9bf3fb797cf2fa65b4",
+    "url": "./static/media/Oswald-regular.31adcb0e.eot"
+  },
+  {
+    "revision": "6d9515af2c14dcad1a6dcaac59ab6363",
+    "url": "./static/media/Oswald-300.6d9515af.ttf"
+  },
+  {
+    "revision": "8573096c5e435cde244a0483601020a2",
+    "url": "./static/media/Oswald-regular.8573096c.ttf"
+  },
+  {
+    "revision": "fe401aaddb1aeff7ed2b8983cfd99dc0",
+    "url": "./static/media/Oswald-300.fe401aad.svg"
+  },
+  {
+    "revision": "48b6c1644fe8b3aa792bc08fdae898ec",
+    "url": "./static/media/Oswald-regular.48b6c164.svg"
+  },
+  {
+    "revision": "4124088fdd8c315a6d096b65b6cbf428",
+    "url": "./static/media/Open-Sans-regular.4124088f.woff2"
+  },
+  {
+    "revision": "d0bad741aefb909e4be56d188b6f02b9",
+    "url": "./static/media/Open-Sans-regular.d0bad741.woff"
+  },
+  {
+    "revision": "c35ad7c76819575781c833319c13711b",
+    "url": "./static/media/Open-Sans-regular.c35ad7c7.eot"
+  },
+  {
+    "revision": "953de7bafd9fdbf41ea443aacabe2706",
+    "url": "./static/media/Open-Sans-regular.953de7ba.ttf"
+  },
+  {
+    "revision": "7e735d7ae17da9ead1360165b1dc3cfb",
+    "url": "./static/media/Open-Sans-regular.7e735d7a.svg"
+  },
+  {
+    "revision": "f19638237704e8b813bdc8a8c966c987",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.452d765951063bfab15152e37d6ea0ed.js"
+  "./precache-manifest.fe94362db3518c51777a3d0ae5fd7b60.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
